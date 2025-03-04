dd30614549b75fe63dacb4aaddfdc5971aedb3a5
Alejandro Soto <94063188+sotoale8@users.noreply.github.com>
2025-03-04 12:19:58 -0500

    update Enemy Behaviour

---
+++ Assets/Script/SpawnManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public Transform[] goalPositions;
+    float timeCount;
+
+    public GameObject ratPrefab;
+
+     void Start()
+    {
+        
+    }
+
+    void Update()
+    {   
+        timeCount+=Time.deltaTime;
+        if(timeCount>10f)//añadir un gamemanager.instance gameStarted; 
+        {
+           int x= Random.Range(0,goalPositions.Length);     
+           Instantiate(ratPrefab,goalPositions[x].transform.position,ratPrefab.transform.rotation); 
+           timeCount=0f;
+        }
+    }
+}
+
+
